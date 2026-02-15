3bea64930504ade02342ca8e782f9c2bad3b3deb
tkboon20 <tkboon20@hotmail.com>
2026-02-15 15:30:33 +0900

    修正

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello GItHub! from Eclipse");
         System.out.println("プッシュ成功したら嬉しいな！");
+        System.out.println("プッシュ成功したら嬉しいな?");
     }
 }
     
