7e53823ead0f0e314ab6f3685debff7933d884ff
tkboo <tkboo@192.168.0.17>
2026-02-15 12:55:20 +0900

    新規作成

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello GItHub! from Eclipse");
+        System.out.println("プッシュ成功したら嬉しいな！");
+    }
+}
+    
